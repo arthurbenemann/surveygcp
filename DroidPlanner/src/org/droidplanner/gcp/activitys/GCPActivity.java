7e92b14bf3748e58e6894041f951491175a4e778
Arthur Benemann <arthur.benemann@gmail.com>
2014-03-26 17:26:58 -0300

    Add back the call to show the settings activity

--- DroidPlanner/src/org/droidplanner/gcp/activitys/GCPActivity.java
+++ DroidPlanner/src/org/droidplanner/gcp/activitys/GCPActivity.java
@@ -52,6 +52,9 @@ public class GCPActivity extends Activity implements OnGcpClickListner {
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
 		case R.id.menu_clear:
 			clearWaypointsAndUpdate();
 			return true;
